c8e575c0220c2b2bf4f6767f2e8932d61bc56a42
tula <mrtula01@gmail.com>
2024-04-16 11:50:17 +0700

    Update base view

--- vue3-fe-layout/src/main.ts
+++ vue3-fe-layout/src/main.ts
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
+import {h, createApp} from "vue";
 import App from './App.vue'
+import singleSpaVue from "single-spa-vue";
+import '@/style.scss'
 
-createApp(App).mount('#app')
+const vueLifecycles = singleSpaVue({
+    createApp,
+    appOptions: {
+        render() {
+            return h(App, {
+                baseUrl: this.baseUrl,
+            });
+        },
+    },
+    handleInstance: (app, propsConfig) => {
+        console.log('propsConfig', propsConfig.baseUrl)
+        app.config.globalProperties.baseUrl = propsConfig.baseUrl;
+        app.config.warnHandler = function (msg, vm, trace) {
+            return null;
+        };
+    },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
